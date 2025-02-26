c9280ff1e4f72cd7cb4757a5b521548714a7acd8
MOHAMMED MOALLEM <dev.moallem@gmail.com>
2025-02-26 23:01:31 +0300

    Things are not working......

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const display = document.getElementById("display");
 let dotable = true;
-let lastInput;
-let lastOperator;
+let preDisplay ="";
+let appednable = true;
+const preD = document.getElementById("pre");
+let operators = ["+","-","/","*"];
 
 function append(input){
     console.log(display.value.length)
@@ -23,17 +25,51 @@ function clearDisplay(){
     display.value = "";
 }
 
+
+
+/* 
+Clears the calculator, and resets any functions. 
+The contents of the memory are not affected by AC. 
+The memory can be cleared after pressing AC by pressing Min. 
+*/
+function clearAll(){
+    display.value = "";
+    preDisplay = "";
+    preD.innerHTML = "";//temp
+
+}
+
+
+
+
 function operate(operator){
-    lastInput = display.value;
-    lastOperator = operator;
+    // if(display.value == undefined) return;
+    preDisplay += display.value;
+    let n = preDisplay.length - 1;
+    if(isOperator(preDisplay.value[n])){
+        console.log(isOperator(preDisplay.value[n]))
+        preDisplay[n] = operator;
+    }else{
+        preDisplay = display.value + operator;
+    }
     clearDisplay();
+    preD.innerHTML = preDisplay;
+    
+    
 }
 
 function cal(){
-    if(lastInput && lastOperator){
-        let str = lastInput + lastOperator + display.value;
-        console.log(str)
-        display.value = eval(str);
+    if(preDisplay){
+        let result = preDisplay + display.value;
+        console.log(result)
+        display.value = eval(result);
         console.log(display.value)
+        preDisplay = display.value;
+        preD.innerHTML = preDisplay;
     }
 }
+
+function isOperator(operator){
+    return operators.includes(operator);
+}
+console.log(isOperator(""))
